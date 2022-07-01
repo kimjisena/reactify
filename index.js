cd540fe895479106b3e278ff70499f5f62dc5f0f
jisena.btc <pjohmaths@gmail.com>
2022-07-01 22:41:20 +0300

    create a directory and files

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hello, reactify!");
+const fs = require('fs');
+
+// create a directory and files
+function dir (name, files) {
+    let path = `./src/components/${name}/`;
+
+    fs.mkdir(path, {recursive: true}, (err) => {
+        if (err) {
+            throw err;
+        }
+
+        for (let f of files) {
+            fs.open((path + f), 'w+', (err, fd) => {
+                if (err) {
+                    throw err;
+                }
+                // todo: add boilerplate
+                fs.close(fd, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
+        }
+    });
+
+    return 0;
+}
+
+module.exports = {
+    dir,
+};
